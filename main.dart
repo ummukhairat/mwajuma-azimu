5e32014b64263f9c9e9dccd18a6f3f710e34997f
Mwajuma <135803317+ummukhairat@users.noreply.github.com>
2024-02-03 14:24:06 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,165 @@
+//the code to display the farmer information
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MarketInfoApp());
+}
+
+class MarketInfoApp extends StatelessWidget {
+  const MarketInfoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Market Information'),
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.grain),
+            tooltip: 'Crops information',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Setting Icon',
+            onPressed: () {},
+          ),
+        ],
+        toolbarHeight: 50,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to Market Information!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              child: const Text(
+                'View Services',
+                style: TextStyle(fontSize: 18),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ServicesPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: const [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+              ),
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.green),
+                accountName:
+                    Text("Uwesu Azimu", style: TextStyle(fontSize: 18)),
+                accountEmail: Text("mwajumaazimu@gmail.com"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Farmer'),
+            ),
+            ListTile(
+              leading: Icon(Icons.agriculture),
+              title: Text('Farm'),
+            ),
+            ListTile(
+              leading: Icon(Icons.attach_money),
+              title: Text('Market'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          fixedColor: Colors.green,
+          items: const [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: "Search",
+              icon: Icon(Icons.search),
+            ),
+            BottomNavigationBarItem(
+              label: "Profile",
+              icon: Icon(Icons.account_circle),
+            ),
+          ],
+          onTap: (int indexOfItem) {}),
+    );
+  }
+}
+
+class ServicesPage extends StatelessWidget {
+  const ServicesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Services'),
+        backgroundColor: Colors.yellow,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Services',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Local & Regional market prices',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Where to sell your crops',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              ' "SORRY, THE APP STILL UNDER DEVELOPMENT"',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        tooltip: 'Go back',
+        child: const Icon(Icons.arrow_back),
+      ),
+    );
+  }
+}
